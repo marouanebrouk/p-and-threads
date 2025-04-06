c5dcd01ca2545db1eab32b048831a1734021c98c
marouanebrouk <socialmarouanebrouk@gmail.com>
2025-04-06 20:52:27 +0100

    yes

--- philosophers/threads.c
+++ philosophers/threads.c
@@ -3,21 +3,35 @@
 #include "stdlib.h"
 #include "pthread.h"
 
-void *ft_routine()
+typedef struct f_threads
 {
+    int number;
+    char *name;
+} t_threads;
+
+void *my_thread(void *arg)
+{
+    t_threads *data = (t_threads *)arg;
     printf("pid si %d\n",getpid());
-    sleep(2);
-    printf("bye thread\n");
+    sleep(60);
+    printf("bye %d thread\n",data->number);
+    printf("bye %s thread\n",(*data).name);
 }
 
 int main()
 {
     pthread_t t1,t2;
-    if (pthread_create(&t1,NULL,&ft_routine,NULL) != 0)
+    t_threads data;
+    data.number = 100;
+    data.name = "marouane";
+    char *str1 = "s";
+    char *str2 = "hamid2";
+    if (pthread_create(&t1,NULL,&my_thread,(void *)&data) != 0)
     {
         return 1; // in case of error
     } //a way to protect the pthread_create function
-    pthread_create(&t2,NULL,&ft_routine,NULL);
+    if (pthread_create(&t2,NULL,&my_thread,(void *)&data) != 0)
+        return 2;
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
 }
