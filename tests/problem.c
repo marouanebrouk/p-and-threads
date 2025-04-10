f6843622c7f5e60ab42c9e823605e20dbddda746
marouanebrouk <socialmarouanebrouk@gmail.com>
2025-04-10 20:51:14 +0100

    help

--- tests/problem.c
+++ tests/problem.c
@@ -2,30 +2,34 @@
 
 int arr[] = {2,3,5,7,11,13,17,19,23,29};
 
+pthread_mutex_t mutex;
+
 void *routine(void * arg)
 {
-    int *i = (int *)arg;
-    printf("%d\n",arr[*i]);
-    sleep(1);
+    int index = *(int *)arg;
+    // pthread_mutex_lock(&mutex);
+    printf("%d\n",arr[index]);
+    // pthread_mutex_unlock(&mutex);
     return(NULL);
 }
 
 
 int main()
 {
+    pthread_mutex_init(&mutex,NULL);
     pthread_t t[10];
-     int i = 0;
-    while (i < 10)
+    for (int i = 0; i < 10; i++)
     {
-        if(pthread_create(t + i,NULL,routine,(void *)&i) != 0)
-            perror("failed to create thread");
-        i++;
+        if(pthread_create(&t[i],NULL,routine,(void *)&i) != 0)
+            perror("failed to create thread\n");
+        usleep(500);
     }
 
     for (int j = 0; j < 10; j++)
     {
             if(pthread_join(t[j],NULL) != 0)
-            return (1);
+                perror("failed to join thread\n");
     }
     return 0;
+    pthread_mutex_destroy(&mutex);
 }
