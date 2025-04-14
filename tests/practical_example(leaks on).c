6245feec9632ad6fa3d4bead926312913277a90e
marouanebrouk <socialmarouanebrouk@gmail.com>
2025-04-14 15:33:45 +0100

    helo

--- tests/practical_example(leaks on).c
+++ tests/practical_example(leaks on).c
@@ -1,18 +1,23 @@
 #include "philo.h"
 int arr[]={2,3,5,7,11,13,17,19,23,29};
+int gsum = 0;
+// pthread_mutex_t mutex;
 
 void *routine(void *arg)
 {
     int i = 0;
-    int *sum = malloc(sizeof(int));
-    int *index = (int *)arg;
+    int  sum = 0;
+    int index = *(int *)arg;
     while (i < 5)
     {
-        *sum = *sum + arr[(*index) + i];
+        // pthread_mutex_lock(&mutex);
+        sum = sum + arr[(index) + i];
         i++;
+        // pthread_mutex_unlock(&mutex);
     }
-    free(index);
-    return (sum);
+    printf("local sum is %d\n",sum);
+    *(int *)arg = sum;
+    return (arg);
 }
 
 int main()
@@ -26,7 +31,9 @@ int main()
         *d = (i * 5);
         if(pthread_create(&t[i],NULL,&routine,(void *)d) != 0)
             perror("failed to create thread\n");
+        printf("thread %d -------------\n",i+1);
         i++;
+        sleep(1);
     }
     i = 0;
     while (i < 2)
@@ -34,7 +41,8 @@ int main()
         if(pthread_join(t[i],(void **)&res))
             perror("error joining the thread\n");
         i++;
+        gsum += *res;
+        // printf("gsum is %d\n",gsum);
     }
-    printf("%d\n",*res);
-    free(res);
+    printf("%d\n",gsum);
 }
