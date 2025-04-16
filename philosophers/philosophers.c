fc726258d060697eaa67f8b0603b698eaf2f9be0
marouanebrouk <socialmarouanebrouk@gmail.com>
2025-04-16 22:39:57 +0100

    helo

--- philosophers/philosophers.c
+++ philosophers/philosophers.c
@@ -1,5 +1,24 @@
 #include "philosophers.h"
 
+
+int ft_atoi(char *str)
+{
+    int sign;
+    int result;
+
+    sign = 1;
+    result = 0;
+    while ((*str >= 9 && *str <= 13 ) || *str == 32)
+        str++;
+    if (*str == '-' || *str == '+' )
+        if (*(str++) == '-')
+            sign = -1;
+    while (*str >= '0' && *str <= '9')
+        result = result * 10 + (*str++ - '0');
+    return(result * sign);
+}
+
+
 int main()
 {
 
