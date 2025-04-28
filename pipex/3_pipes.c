a02a0bce708e8d0d2d9a2d63b77cbbed4935f087
marouanebrouk <marouanebrouk@gmail.com>
2025-04-28 02:28:23 +0100

    fixed pipe3

--- pipex/3_pipes.c
+++ pipex/3_pipes.c
@@ -8,9 +8,9 @@
 int main()
 {
     int pid1,pid2,pid3;
-    int fds[2];
-    int fds2[2];
-    if (pipe(fds) == -1)
+    int pipe1[2];
+    int pipe2[2];
+    if (pipe(pipe1) == -1)
     {
         perror("pipe1");
         exit(1);
@@ -18,15 +18,15 @@ int main()
     pid1 = fork();
     if (pid1 == 0)
     {
-        close(fds[0]);
-        dup2(fds[1],STDOUT_FILENO);
-        close(fds[1]);
+        close(pipe1[0]);
+        dup2(pipe1[1],STDOUT_FILENO);
+        close(pipe1[1]);
         char *args[] = {"/bin/ls",NULL};
         execv("/bin/ls",args);
         perror("execv ls");
         exit(1);
     }
-    if (pipe(fds2) == -1)
+    if (pipe(pipe2) == -1)
     {
         perror("pipe2");
         exit(1);
@@ -39,13 +39,15 @@ int main()
         exit(1);
     }
     if (pid2 == 0)
-    {
-        close(fds[1]);
-        close(fds2[0]);
-        dup2(fds[0],STDIN_FILENO);
-        dup2(fds2[1],STDOUT_FILENO);
-        close(fds[0]);
-        close(fds2[1]);
+    {   // ls | grep a.out | wc -l
+        close(pipe1[1]);
+        dup2(pipe1[0],STDIN_FILENO);
+        close(pipe1[0]);
+
+        close(pipe2[0]);
+        dup2(pipe2[1],STDOUT_FILENO);
+        close(pipe2[1]);
+
         char *args2[] = {"/bin/grep","a.out",NULL};
         execv("/bin/grep",args2);
         perror("execv");
@@ -58,21 +60,24 @@ int main()
         exit(1);
     }
     if (pid3 == 0)
-    {
-        close(fds2[1]);
-        dup2(fds2[0],STDIN_FILENO);
-        close(fds2[0]);
-        char *args3[] = {"/bin/wc","-l",NULL};
+    { // ls | grep a.out | wc -l
+        close(pipe2[1]);
+        close(pipe1[1]);
+        close(pipe1[0]);
+        dup2(pipe2[0],STDIN_FILENO);
+        close(pipe2[0]);
+        
+        char *args3[] = {"usr/bin/wc","-l",NULL};
         execv("/bin/wc",args3);
         perror("execv wc");
         exit(1);
     }
     else
     {
-        close(fds[0]);
-        close(fds[1]);
-        close(fds2[0]);
-        close(fds2[1]);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
         wait(NULL);
         wait(NULL);
         wait(NULL);
