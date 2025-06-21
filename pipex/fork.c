a4664c23424d9e71513ce8acd0a9604153614dc6
marouanebrouk <socialmarouanebrouk@gmail.com>
2025-06-21 21:00:06 +0100

    added a file

---
+++ pipex/fork.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int id = fork();
+    int n;
+    if (id == 0)
+        n = 1;
+    else
+        n = 6;
+    if (id != 0)
+        wait(NULL);
+    for (int i = n; i < n + 5 ;i++)
+    {
+        printf("%d ",i);
+        fflush(stdout);
+    }
+    if (id != 0)
+        printf("\n");
+}
